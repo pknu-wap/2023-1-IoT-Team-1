42363b291f3bca2a1f33f372205ddaf42ab5e4f3
Zepelown <dbswjdqhd12@gmail.com>
2023-04-04 20:50:56 +0900

    test commit

--- app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/GraphFragment.kt
+++ app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/GraphFragment.kt
@@ -12,11 +12,6 @@ import com.example.fishfarmapplication.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GraphFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GraphFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
--- app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/MypageFragment.kt
+++ app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/MypageFragment.kt
@@ -12,11 +12,7 @@ import com.example.fishfarmapplication.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MypageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class MypageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
