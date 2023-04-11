3b05caf3a0d5ede26bc67f38b7de1e2d0f125029
SeongWooHong <pluto0303@naver.com>
2023-04-11 11:07:26 +0900

    Ultra sensor test

---
+++ ultra/ultra.ino
@@ -0,0 +1,40 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-rgb-led
+ */
+
+#define PIN_RED    23 // GIOP23
+#define PIN_GREEN  22 // GIOP22
+#define PIN_BLUE   21 // GIOP21
+
+void setup() {
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
+}
+
+void loop() {
+  // color code #00C9CC (R = 0,   G = 201, B = 204)
+  analogWrite(PIN_RED,   0);
+  analogWrite(PIN_GREEN, 201);
+  analogWrite(PIN_BLUE,  204);
+
+  delay(1000); // keep the color 1 second
+
+  // color code #F7788A (R = 247, G = 120, B = 138)
+  analogWrite(PIN_RED,   247);
+  analogWrite(PIN_GREEN, 120);
+  analogWrite(PIN_BLUE,  138);
+
+  delay(1000); // keep the color 1 second
+
+  // color code #34A853 (R = 52,  G = 168, B = 83)
+  analogWrite(PIN_RED,   52);
+  analogWrite(PIN_GREEN, 168);
+  analogWrite(PIN_BLUE,  83);
+
+  delay(1000); // keep the color 1 second
+}
