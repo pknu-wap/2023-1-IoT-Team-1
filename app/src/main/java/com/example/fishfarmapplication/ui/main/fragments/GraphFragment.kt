5d410a11c33b40e00af0e8280caec94b969f7fcb
SHyeonL <kjghost1@naver.com>
2023-05-02 00:27:45 +0900

    그래프 값 파이어베이스 연동 & 실시간으로 주고받기

--- app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/GraphFragment.kt
+++ app/src/main/java/com/example/fishfarmapplication/ui/main/fragments/GraphFragment.kt
@@ -1,62 +1,108 @@
 package com.example.fishfarmapplication.ui.main.fragments
 
+import android.content.ContentValues.TAG
 import android.graphics.Color
+import android.graphics.PostProcessor
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Debug
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import com.example.fishfarmapplication.R
+import androidx.fragment.app.Fragment
 import com.example.fishfarmapplication.databinding.FragmentGraphBinding
 import com.example.fishfarmapplication.ui.main.MainViewModel
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.lang.String
+import kotlin.Float
+import kotlin.Int
 
 class GraphFragment : Fragment() {
-    private lateinit var viewModel:MainViewModel
-    private lateinit var binding : FragmentGraphBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentGraphBinding
+
+    private val database =
+        Firebase.database("https://wap-iot-9494c-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    private val myRef = database.getReference("sensor")
+    val entries = ArrayList<Entry>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentGraphBinding.inflate(inflater, container,false)
+        binding = FragmentGraphBinding.inflate(inflater, container, false)
         val chart = binding.testChart
-        val entries = ArrayList<Entry>()
-        entries.add(Entry(1F, 1.2F))
-        entries.add(Entry(2F, 2F))
-        entries.add(Entry(4F, 1.8F))
-        entries.add(Entry(5F, 3F))
-        entries.add(Entry(6F, 2F))
 
-        val lineDataSet = LineDataSet(entries, "속성명1")
-        lineDataSet.lineWidth = 2f
-        lineDataSet.circleRadius = 6f
-        lineDataSet.setDrawCircleHole(true)
-        lineDataSet.setDrawCircles(true)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                entries.clear()
+                var i = 1
+                for (item in snapshot.children) {
+                    val test: Float = String.valueOf(item.value).toFloat()
+                    entries.add(Entry(i.toFloat(), test))
+                    i += 1
+                }
 
-        val xAxis = chart.xAxis
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.textColor = Color.BLACK
-        xAxis.enableGridDashedLine(8F,24F,0F)
+                val lineDataSet = LineDataSet(entries, "속성명1")
+                lineDataSet.lineWidth = 2f
+                lineDataSet.circleRadius = 6f
+                lineDataSet.setDrawCircleHole(true)
+                lineDataSet.setDrawCircles(true)
 
-        val yLAxis = chart.axisLeft
-        yLAxis.textColor = Color.BLACK
+                val xAxis = chart.xAxis
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+                xAxis.textColor = Color.BLACK
+                xAxis.enableGridDashedLine(8F, 24F, 0F)
 
-        val yRAxis = chart.axisRight
-        yRAxis.setDrawLabels(false)
-        yRAxis.setDrawAxisLine(false)
-        yRAxis.setDrawGridLines(false)
+                val yLAxis = chart.axisLeft
+                yLAxis.textColor = Color.BLACK
 
-        val lineData = LineData(lineDataSet)
-        chart.data = lineData
+                val yRAxis = chart.axisRight
+                yRAxis.setDrawLabels(false)
+                yRAxis.setDrawAxisLine(false)
+                yRAxis.setDrawGridLines(false)
 
-        chart.invalidate()
+                val lineData = LineData(lineDataSet)
+                chart.data = lineData
 
+                chart.invalidate()
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                print(error.message)
+            }
+        })
 
         return binding.root
+    }
+
+    fun loadMsgs() {
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                entries.clear()
+                var i = 1
+                for (item in snapshot.children) {
+                    Log.d("파이어베이스", item.value.toString())
+                    Log.d("파이어베이스", item.toString())
+                    val test: Float = String.valueOf(item.value).toFloat()
+                    entries.add(Entry(i.toFloat(), test))
+                    i += 1
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                print(error.message)
+            }
+        })
     }
 }
